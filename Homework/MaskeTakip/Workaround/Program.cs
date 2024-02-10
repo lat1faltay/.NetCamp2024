fe9c0a3a063e4e55cbb18e325ae1d1cde0ac0440
Latif Altay <latifaltay00@gmail.com>
2024-02-10 03:46:28 +0300

    Homework 1 is over

---
+++ Homework/MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,96 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer("Latif");
+            SelamVer("Ali");
+            SelamVer("Ayşe");
+            SelamVer();
+
+            int sonuc = Topla(5, 58);
+
+
+            string ogrenci1 = "Latif";
+            string ogrenci2 = "Ahmet";
+            string ogrenci3 = "Mehmet";
+
+
+            //Console.WriteLine(ogrenci1);
+            //Console.WriteLine(ogrenci2);
+            //Console.WriteLine(ogrenci3);
+
+            // Diziler / Arrays
+            Console.WriteLine();
+            Console.WriteLine("Array");
+            Console.WriteLine();
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Latif";
+            ogrenciler[1] = "Ahmet";
+            ogrenciler[2] = "Mehmet";
+
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "İstanbul", "Ankara", "İzmir" };
+            string[] sehirler2 = new[] { "Sakarya", "Antalya", "Bursa" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+            //sayi2 = sayi1;
+            //sayi1 = 30;
+
+
+            Person person1 = new Person();
+            person1.FirstName = "Murat";
+
+            Person person2 = new Person();
+            person1.FirstName = "Teoman";
+
+
+            foreach (string sehir in sehirler2) 
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yeniSehirler1 = new List<string> { "Ankara", "İstanbul", "İzmir" };
+            yeniSehirler1.Add("İzmit");
+
+            foreach (string yeniSehir in yeniSehirler1) 
+            {
+                Console.WriteLine(yeniSehir);
+            }
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim="isimsiz") 
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2= 10) 
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            return sonuc;
+        }
+    }
+}
--- Intro/Intro/obj/Debug/net8.0/Intro.AssemblyInfo.cs
+++ Intro/Intro/obj/Debug/net8.0/Intro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6295e5773fb9a496418e44e86854247f64a40f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
